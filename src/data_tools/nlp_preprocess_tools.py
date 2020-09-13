6140f672527aac250099ebaa73c98c23740612fb
fxxuuu <827526716@qq.com>
2020-09-13 21:07:33 +0800

    Create nlp_preprocess_tools.py

---
+++ src/data_tools/nlp_preprocess_tools.py
@@ -0,0 +1,19 @@
+import unicodedata
+import string
+
+
+'''
+Unicode 转 ASCII
+'''
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+
+# Turn a Unicode string to plain ASCII, thanks to https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+print(unicodeToAscii('Ślusàrski'))
